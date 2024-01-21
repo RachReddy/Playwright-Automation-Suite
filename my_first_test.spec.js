42878c5bc2feba43962039db064b37b0f5291219
Rachana NV <94166047+RachReddy@users.noreply.github.com>
2024-01-21 19:52:55 +0530

    Update my_first_test.spec.js

--- my_first_test.spec.js
+++ my_first_test.spec.js
@@ -1,9 +1,5 @@
 const {test,expect} = require('@playwright/test')
 
-//const {hello, helloworld} = require('./demo/hello')
-
-//console.log(helloworld());
-
 test('My first test', async ({page}) =>{
 
    await page.goto('https://google.com')
